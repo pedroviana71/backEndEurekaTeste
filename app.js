4174e53064e3cd5837a4bff60782497dcbc7b218
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-10-05 21:15:46 -0300

    fixed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const axios = require("axios");
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
