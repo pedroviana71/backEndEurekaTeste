592768d015bc290635df72cea4c2f6a941738631
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-10-07 22:28:57 -0300

    redis complete implemented

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const express = require("express");
-const redis = require("redis");
 const app = express();
-const client = redis.createClient();
-
 const axios = require("axios");
-
 var cors = require("cors");
+
+const redis = require("redis");
+const client = redis.createClient();
 
 const port = 3001;
 
 app.use(cors());
-
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  const { cep } = req.query;
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to Redis");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/api", async (req, res) => {
+  res.send("API is working");
+});
+
+app.get("/api/:cep", async (req, res) => {
+  const { cep } = req.params;
+  const data = await client.get(cep);
   try {
-    client
-      .connect()
-      .then(() => {
-        console.log("Connected to Redis");
-        client.set("visits", "550");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    const teste = await client.get("visits");
-    console.log(teste);
-    const data = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-    return res.json(data.data);
+    if (data) {
+      return res.send(JSON.parse(data));
+    }
+    const address = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    client.set(cep, JSON.stringify(address.data));
+    return res.json(address.data);
   } catch (error) {
     console.log(error);
     return res.json({ error: "CEP inválido" });
